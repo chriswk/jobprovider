10ca06fe9318e4c2d670dd70e38b47242f9931df
chrkolst <christopher.kolstad@finn.no>
2013-09-23 12:17:23 +0200

    Setup CORS to allow *

--- server.js
+++ server.js
@@ -14,8 +14,18 @@ var ads = [{
 		orgname: "FINN.no" 
 }];
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', ['Content-Type', "X-Requested-With"]);
+
+    next();
+}
+
+
+app.all("/*", allowCrossDomain);
+
 app.get('/json', function (req, res) {
-	
 	if (req.query.callback && req.query.callback != "") {
 		res.jsonp({ ads: ads})
 	} else {
@@ -28,7 +38,9 @@ app.get('/xml', function (req, res) {
 	res.send(simplexml.toXML(ads));
 	res.send(200);
 });
+
 var port = process.env.PORT || 5000
+
 app.listen(port, function () {
 	console.log("Listening on port: " +port);
 });
